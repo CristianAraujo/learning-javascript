607916213a35cfc789a64f7c63442ae84b501589
Cristian Araujo <cristianaraujoaraujo@gmail.com>
2022-12-01 14:23:17 -0300

    Se construye iterador que realiza filtro - Función que recibe un iterable, filtra los elementos que cumplen   con una determinda condición y luego devuelve un objeto iterable   con el resultado.

---
+++ Generators/tests/js/iterFilter.js
@@ -0,0 +1,22 @@
+// Fecha: 01 de diciembre del 2022
+
+function iterFilter (iterable, filtro) {
+    let iterador = iterable[Symbol.iterator]();
+
+    return {
+        [Symbol.iterator]() { return this; },
+        
+        next(){
+            for (;;) {
+                let v = iterador.next();
+                if (v.done || filtro(v.value)) {
+                    return v;
+                }
+            }
+        }
+    };
+}
+
+// Implementación
+let resultado = [...iterFilter([1,2,3,4,5,6,7,8,9,10], x => x % 2 == 0)]
+console.log(resultado);
