99401a83ca06b015b6a11a81e187e25438e48880
Cristian Araujo <cristianaraujoaraujo@gmail.com>
2022-12-01 15:32:37 -0300

    Sección generadores - Se crea un ejemplo de ggnerador - Se crea archivo de toeria de generadores

---
+++ Generators/tests/js/simpeGenerator.js
@@ -0,0 +1,34 @@
+// Fecha: 01 de diciembre del 2022
+
+/**
+ * Se crea una función generadora
+ */
+
+function* generador() {
+    yield 2;
+    yield 3;
+    yield 5;
+    yield 7;
+}
+
+let primos = generador();
+console.log(primos);
+
+console.log(primos.next())
+console.log(primos.next())
+console.log(primos.next())
+console.log(primos.next())
+console.log(primos.next())
+
+
+/**
+ * Implementación de un generador expresado
+ */
+
+let secuencia = function* (from, to) {
+    for (let i = from; i <= to; i++) { yield i; }
+}
+
+let seq = secuencia(5,9);
+console.log(seq);
+console.log([...seq]);
